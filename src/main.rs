e263798dde6a5df7631d0e6693ac1973fe3fa17f
kuviman <kuviman@gmail.com>
2021-04-04 18:55:58 +0400

    Set current dir to exe dir outside cargo

--- src/main.rs
+++ src/main.rs
@@ -920,9 +920,17 @@ impl Drop for GameState {
 
 fn main() {
     geng::setup_panic_handler();
-    if let Ok(dir) = std::env::var("CARGO_MANIFEST_DIR") {
+    if let Some(dir) = std::env::var_os("CARGO_MANIFEST_DIR") {
         std::env::set_current_dir(std::path::Path::new(&dir).join("static")).unwrap();
+    } else {
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            if let Some(path) = std::env::current_exe().unwrap().parent() {
+                std::env::set_current_dir(path).unwrap();
+            }
+        }
     }
+
     let geng = Rc::new(Geng::new(default()));
     let assets = <Assets as geng::LoadAsset>::load(&geng, ".");
     geng::run(
