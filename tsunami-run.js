9695099fd2a33e8bf1323acb5bf7005c1a07a166
kuviman <kuviman@users.noreply.github.com>
2021-04-04 09:22:02 +0000

    deploy: ad23f218cb102e5385af3d5db84063afd264a64b

--- tsunami-run.js
+++ tsunami-run.js
@@ -889,23 +889,23 @@ async function init(input) {
         var ret = makeMutClosure(arg0, arg1, 129, __wbg_adapter_26);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper594 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper596 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 265, __wbg_adapter_29);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper596 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper598 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 265, __wbg_adapter_32);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper598 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper600 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 265, __wbg_adapter_35);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper600 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper602 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 265, __wbg_adapter_38);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper602 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper604 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 265, __wbg_adapter_41);
         return addHeapObject(ret);
     };
